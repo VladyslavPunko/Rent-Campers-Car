3769d8c4b8e6f80055eb4ce95ada2cb0f84e0666
VladyslavPunko <143653897+VladyslavPunko@users.noreply.github.com>
2024-08-29 21:55:22 +0200

    Home page

--- src/page/HomePage/Homepage.jsx
+++ src/page/HomePage/Homepage.jsx
@@ -1,5 +1,16 @@
+import css from "./Homepage.module.css";
 const Homepage = () => {
-  return <div></div>;
+  return (
+    <div className={css.container}>
+      <h1 className={css.uptext}>Campers of your dreams</h1>
+      <h2 className={css.downtext}>
+        You can find everything you want in our catalog
+      </h2>
+      <button type="button" className={css.button}>
+        View Now
+      </button>
+    </div>
+  );
 };
 
 export default Homepage;

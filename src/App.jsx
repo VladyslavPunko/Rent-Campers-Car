59fd71967ad35147301cb487212d2b8e2df18249
VladyslavPunko <143653897+VladyslavPunko@users.noreply.github.com>
2024-08-29 13:53:20 +0200

    Navigate + routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import { Suspense } from "react";
+import Navigation from "./components/Navigation/Navigation.jsx";
+import Loader from "./components/Loader/Loader.jsx";
+import { Route, Routes } from "react-router-dom";
+
+import Homepage from ".//page/HomePage/Homepage.jsx";
+import CatalogPage from "./page/CatalogPage/CatalogPage.jsx";
+import DetailPage from "./page/DetailPage/DetailPage.jsx";
+import NotFoundPage from "./page/NotFoundPage/NotFound.jsx";
+
 function App() {
   return (
     <>
-      <p>привет нахуй</p>
+      <header>
+        <Navigation />
+      </header>
+      <main>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/" element={<Homepage />}></Route>
+            <Route path="/catalog" element={<CatalogPage />}></Route>
+            <Route path="/catalog/:id" element={<DetailPage />} />
+            <Route path="*" element={<NotFoundPage />}></Route>
+          </Routes>
+        </Suspense>
+      </main>
     </>
   );
 }
---
+++ src/page/NotFoundPage/NotFound.jsx
@@ -0,0 +1,7 @@
+import css from "./NotFound.module.css";
+
+const NotFoundPage = () => {
+  return <div className={css.no_page}>Wooops, page is not found!</div>;
+};
+
+export default NotFoundPage;
